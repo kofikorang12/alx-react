bcdd8baaf2eab2eb722d861125e906837d2b8dc1
Kofi korang <jbaah69@gmail.com>
2024-04-02 14:15:03 +0000

    Setting up development server, with code divided into modules

---
+++ 0x00-Webpack/task_3/js/dashboard_main.js
@@ -0,0 +1,19 @@
+const $ = required('jquery');
+const _ = required('lodash');
+
+import '../css/main.css';
+
+let counter = 0;
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} click on the button`);
+}
+
+$("button").on("click", _.debounce(updateCounter, 500));
